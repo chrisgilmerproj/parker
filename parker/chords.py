d2605bc8a2296c1755f031f35088a87480feb7bb
Chris Gilmer <chris.gilmer@gmail.com>
2016-03-24 10:31:01 -0700

    Remove all chord constructor classmethods from Chord() class

--- parker/chords.py
+++ parker/chords.py
@@ -1,9 +1,101 @@
 from .constants import CHORD_MATCHER
 from .notes import Note
 from .notes import NoteGroupBase
-from .notes import NotesParser
 from .mixins import Aug
 from .mixins import Dim
+
+
+SHORTHAND = {
+    # Triads
+    'm': [0, 3, 7],
+    'M': [0, 4, 7],
+    'dim': [0, 3, 6],
+    # Augmented
+    'aug': [0, 4, Aug(7)],
+    'm7+': [0, 4, Aug(7), 10],
+    'M7+': [0, 4, Aug(7), 11],
+    # Suspended
+    'sus47': [0, 5, 7, 10],
+    'sus4': [0, 5, 7],
+    'sus2': [0, 2, 7],
+    'sus4b9': [0, 5, 7, 13],
+    # Sevenths
+    'm7': [0, 3, 7, 10],
+    'M7': [0, 4, 7, 11],
+    '7': [0, 4, 7, 10],
+    'm7b5': [0, 3, 6, 10],
+    'dim7': [0, 3, 6, Dim(10)],
+    'mM7': [0, 3, 7, 11],
+    # Sixths
+    'm6': [0, 3, 7, 9],
+    'M6': [0, 4, 7, 9],
+    '67': [0, 4, 7, 9, 10],
+    # Ninths
+    '69': [0, 4, 7, 9, 14],
+    '9': [0, 4, 7, 10, 14],
+    '7b9': [0, 4, 7, 10, 13],
+    '7#9': [0, 4, 7, 10, Aug(14)],
+    'M9': [0, 4, 7, 11, 14],
+    'm9': [0, 3, 7, 10, 14],
+    'add9': [0, 4, 7, 14],
+    # Elevenths
+    '11': [0, 7, 10, 17],
+    'm11': [0, 3, 7, 10, 17],
+    '7#11': [0, 4, 7, 10, Aug(17)],
+    # Thirteenths
+    'M13': [0, 4, 7, 11, 14, 21],
+    'm13': [0, 3, 7, 10, 14, 21],
+    '13': [0, 4, 7, 10, 14, 21],
+    # Altered chords
+    '7b5': [0, 4, 6, 10],
+    '7b12': [0, 4, 7, 10, 15],
+}
+
+CHORD_TO_SHORTHAND = {
+    # Triads
+    'minor_triad': 'm',
+    'major_triad': 'M',
+    'diminished_triad': 'dim',
+    # Augmented
+    'augmented_triad': 'aug',
+    'augmented_minor_seventh': 'm7+',
+    'augmented_major_seventh': 'M7+',
+    # Suspended
+    'suspended_fourth_triad': 'sus4',
+    'suspended_second_triad': 'sus2',
+    'suspended_seventh': 'sus47',
+    'suspended_fourth_ninth': 'sus4b9',
+    # Sevenths
+    'minor_seventh': 'm7',
+    'major_seventh': 'M7',
+    'dominant_seventh': '7',
+    'minor_seventh_flat_five': 'm7b5',
+    'diminished_seventh': 'dim7',
+    'minor_major_seventh': 'mM7',
+    # Sixths
+    'minor_sixth': 'm6',
+    'major_sixth': 'M6',
+    'dominant_sixth': '67',
+    # Ninths
+    'sixth_ninth': '69',
+    'dominant_ninth': '9',
+    'dominant_flat_ninth': '7b9',
+    'dominant_sharp_ninth': '7#9',
+    'major_ninth': 'M9',
+    'minor_ninth': 'm9',
+    'added_ninth': 'add9',
+    # Elevenths
+    'eleventh': '11',
+    'minor_eleventh': 'm11',
+    'lydian_dominant_seventh': '7#11',
+    # Thirteenths
+    'major_thirteenth': 'M13',
+    'minor_thirteenth': 'm13',
+    'dominant_thirteenth': '13',
+    # Altered chords
+    'dominant_flat_five': '7b5',
+    'hendrix_chord': '7b12',
+}
 
 
 class Chord(NoteGroupBase):
@@ -11,24 +103,16 @@ class Chord(NoteGroupBase):
     Source Material: https://en.wikipedia.org/wiki/Chord_(music)
     """
 
-    def __init__(self, chord=None, notes=None, extension=None, octave=None):
+    def __init__(self, chord=None, octave=None):
         """
-        Create chords either from a chord name or from a set
-        of notes.  The notes should be a list of Note() objects.
-        The extension is optional and helpful only for string
-        representation.
+        Create chords from a chord name and an optional octave.
         """
-        self.notes = []
-        self.extension = ''
         self._octave = octave
         if isinstance(chord, str):
             self._set_from_string(chord)
-        elif notes:
-            self.notes.extend(NotesParser.parse(notes))
-            self.extension = extension or ''
         else:
-            raise Exception('Must instantiate Chord() object with a chord '
-                            'name or a set of notes in a list')
+            raise Exception('Must instantiate Chord() object with a '
+                            'chord name')
 
     def __str__(self):
         return '{}{}'.format(self.notes[0].generalize() if self.notes else '',
@@ -50,9 +134,8 @@ class Chord(NoteGroupBase):
             root = "{}{}".format(root, self._octave)
         self.extension = self.normalize_extension(extension)
 
-        chord = self._get_shorthand(self.extension)(root)
-        self.notes = chord.notes
-        self.extension = chord.extension
+        transpose_list = self._get_shorthand(self.extension)
+        self.notes = Note(root).transpose_list(transpose_list)
 
     @staticmethod
     def normalize_extension(extension):
@@ -64,216 +147,7 @@ class Chord(NoteGroupBase):
         return extension
 
     @classmethod
-    def _create_chord(cls, root, transpose_list, extension=None):
-        return cls(notes=Note(root).transpose_list(transpose_list),
-                   extension=extension)
-
-    @classmethod
-    def major_triad(cls, root):
-        return cls._create_chord(root, [0, 4, 7], 'M')
-
-    @classmethod
-    def minor_triad(cls, root):
-        return cls._create_chord(root, [0, 3, 7], 'm')
-
-    @classmethod
-    def diminished_triad(cls, root):
-        return cls._create_chord(root, [0, 3, 6], 'dim')
-
-    @classmethod
-    def augmented_triad(cls, root):
-        return cls._create_chord(root, [0, 4, Aug(7)], 'aug')
-
-    @classmethod
-    def augmented_minor_seventh(cls, root):
-        return cls._create_chord(root, [0, 4, Aug(7), 10], 'm7+')
-
-    @classmethod
-    def augmented_major_seventh(cls, root):
-        return cls._create_chord(root, [0, 4, Aug(7), 11], 'M7+')
-
-    @classmethod
-    def suspended_triad(cls, root):
-        return cls._create_chord(root, [0, 5, 7], 'sus')
-
-    @classmethod
-    def suspended_fourth_triad(cls, root):
-        return cls._create_chord(root, [0, 5, 7], 'sus4')
-
-    @classmethod
-    def suspended_second_triad(cls, root):
-        return cls._create_chord(root, [0, 2, 7], 'sus2')
-
-    @classmethod
-    def suspended_seventh(cls, root):
-        return cls._create_chord(root, [0, 5, 7, 10], 'sus47')
-
-    @classmethod
-    def suspended_fourth_ninth(cls, root):
-        return cls._create_chord(root, [0, 5, 7, 13], 'sus4b9')
-
-    @classmethod
-    def major_seventh(cls, root):
-        return cls._create_chord(root, [0, 4, 7, 11], 'M7')
-
-    @classmethod
-    def minor_seventh(cls, root):
-        return cls._create_chord(root, [0, 3, 7, 10], 'm7')
-
-    @classmethod
-    def dominant_seventh(cls, root):
-        return cls._create_chord(root, [0, 4, 7, 10], 'dom7')
-
-    @classmethod
-    def diminished_seventh(cls, root):
-        return cls._create_chord(root, [0, 3, 6, Dim(10)], 'dim7')
-
-    @classmethod
-    def half_diminished_seventh(cls, root):
-        return cls._create_chord(root, [0, 3, 6, 10], 'm7b5')
-
-    @classmethod
-    def minor_seventh_flat_five(cls, root):
-        return cls._create_chord(root, [0, 3, 6, 10], 'm7b5')
-
-    @classmethod
-    def minor_major_seventh(cls, root):
-        return cls._create_chord(root, [0, 3, 7, 11], 'm/M7')
-
-    @classmethod
-    def minor_sixth(cls, root):
-        return cls._create_chord(root, [0, 3, 7, 9], 'm6')
-
-    @classmethod
-    def major_sixth(cls, root):
-        return cls._create_chord(root, [0, 4, 7, 9], 'M6')
-
-    @classmethod
-    def dominant_sixth(cls, root):
-        return cls._create_chord(root, [0, 4, 7, 9, 10], '67')
-
-    @classmethod
-    def sixth_ninth(cls, root):
-        return cls._create_chord(root, [0, 4, 7, 9, 14], '69')
-
-    @classmethod
-    def minor_ninth(cls, root):
-        return cls._create_chord(root, [0, 3, 7, 10, 14], 'm9')
-
-    @classmethod
-    def major_ninth(cls, root):
-        return cls._create_chord(root, [0, 4, 7, 11, 14], 'M9')
-
-    @classmethod
-    def dominant_ninth(cls, root):
-        return cls._create_chord(root, [0, 4, 7, 10, 14], '9')
-
-    @classmethod
-    def dominant_flat_ninth(cls, root):
-        return cls._create_chord(root, [0, 4, 7, 10, 13], '7b9')
-
-    @classmethod
-    def dominant_sharp_ninth(cls, root):
-        return cls._create_chord(root, [0, 4, 7, 10, Aug(14)], '7#9')
-
-    @classmethod
-    def added_ninth(cls, root):
-        return cls._create_chord(root, [0, 4, 7, 14], 'add9')
-
-    @classmethod
-    def eleventh(cls, root):
-        return cls._create_chord(root, [0, 7, 10, 17], '11')
-
-    @classmethod
-    def minor_eleventh(cls, root):
-        return cls._create_chord(root, [0, 3, 7, 10, 17], 'm11')
-
-    @classmethod
-    def lydian_dominant_seventh(cls, root):
-        return cls._create_chord(root, [0, 4, 7, 10, Aug(17)], '7#11')
-
-    @classmethod
-    def minor_thirteenth(cls, root):
-        return cls._create_chord(root, [0, 3, 7, 10, 14, 21], 'm13')
-
-    @classmethod
-    def major_thirteenth(cls, root):
-        return cls._create_chord(root, [0, 4, 7, 11, 14, 21], 'M13')
-
-    @classmethod
-    def dominant_thirteenth(cls, root):
-        return cls._create_chord(root, [0, 4, 7, 10, 14, 21], '13')
-
-    @classmethod
-    def dominant_flat_five(cls, root):
-        return cls._create_chord(root, [0, 4, 6, 10], '7b5')
-
-    @classmethod
-    def hendrix_chord(cls, root):
-        return cls._create_chord(root, [0, 4, 7, 10, 15], '7b12')
-
-    @classmethod
     def _get_shorthand(cls, shorthand):
-        SHORTHAND = {
-            # Triads
-            'm': cls.minor_triad,
-            'M': cls.major_triad,
-            '': cls.major_triad,
-            'o': cls.diminished_triad,
-            'dim': cls.diminished_triad,
-            # Augmented
-            'aug': cls.augmented_triad,
-            '+': cls.augmented_triad,
-            '7#5': cls.augmented_minor_seventh,
-            'M7+5': cls.augmented_minor_seventh,
-            'M7+': cls.augmented_major_seventh,
-            'm7+': cls.augmented_minor_seventh,
-            '7+': cls.augmented_major_seventh,
-            # Suspended
-            'sus47': cls.suspended_seventh,
-            'sus4': cls.suspended_fourth_triad,
-            'sus2': cls.suspended_second_triad,
-            'sus': cls.suspended_triad,
-            'sus4b9': cls.suspended_fourth_ninth,
-            'susb9': cls.suspended_fourth_ninth,
-            # Sevenths
-            'm7': cls.minor_seventh,
-            'M7': cls.major_seventh,
-            '7': cls.dominant_seventh,
-            'dom7': cls.dominant_seventh,
-            'm7b5': cls.minor_seventh_flat_five,
-            # 'm7b5': cls.half_diminished_seventh,
-            'dim7': cls.diminished_seventh,
-            'm/M7': cls.minor_major_seventh,
-            'mM7': cls.minor_major_seventh,
-            # Sixths
-            'm6': cls.minor_sixth,
-            'M6': cls.major_sixth,
-            '6': cls.major_sixth,
-            '6/7': cls.dominant_sixth,
-            '67': cls.dominant_sixth,
-            # Ninths
-            '6/9': cls.sixth_ninth,
-            '69': cls.sixth_ninth,
-            '9': cls.dominant_ninth,
-            '7b9': cls.dominant_flat_ninth,
-            '7#9': cls.dominant_sharp_ninth,
-            'M9': cls.major_ninth,
-            'm9': cls.minor_ninth,
-            'add9': cls.added_ninth,
-            # Elevenths
-            '11': cls.eleventh,
-            'm11': cls.minor_eleventh,
-            '7#11': cls.lydian_dominant_seventh,
-            # Thirteenths
-            'M13': cls.major_thirteenth,
-            'm13': cls.minor_thirteenth,
-            '13': cls.dominant_thirteenth,
-            # Altered chords
-            '7b5': cls.dominant_flat_five,
-            'hendrix': cls.hendrix_chord,
-            '7b12': cls.hendrix_chord,
-        }
         if shorthand not in SHORTHAND:
             raise Exception("Unknown chord extensions: {}".format(shorthand))
         return SHORTHAND[shorthand]
@@ -287,55 +161,9 @@ def produce_all_chords(root):
     as the value.
     """
 
-    cls = Chord
-    chord_methods = [
-        # Triads
-        cls.minor_triad,
-        cls.major_triad,
-        cls.diminished_triad,
-        # Augmented
-        cls.augmented_triad,
-        cls.augmented_minor_seventh,
-        cls.augmented_major_seventh,
-        # Suspended
-        cls.suspended_fourth_triad,
-        cls.suspended_second_triad,
-        cls.suspended_seventh,
-        cls.suspended_triad,
-        cls.suspended_fourth_ninth,
-        # Sevenths
-        cls.minor_seventh,
-        cls.major_seventh,
-        cls.dominant_seventh,
-        cls.minor_seventh_flat_five,
-        cls.half_diminished_seventh,
-        cls.diminished_seventh,
-        cls.minor_major_seventh,
-        # Sixths
-        cls.minor_sixth,
-        cls.major_sixth,
-        cls.dominant_sixth,
-        # Ninths
-        cls.sixth_ninth,
-        cls.dominant_ninth,
-        cls.dominant_flat_ninth,
-        cls.dominant_sharp_ninth,
-        cls.major_ninth,
-        cls.minor_ninth,
-        # Elevenths
-        cls.eleventh,
-        cls.minor_eleventh,
-        cls.lydian_dominant_seventh,
-        # Thirteenths
-        cls.major_thirteenth,
-        cls.minor_thirteenth,
-        cls.dominant_thirteenth,
-        # Altered chords
-        cls.dominant_flat_five,
-        cls.hendrix_chord,
-    ]
     chord_info = {}
-    for method in chord_methods:
-        chord = method(root)
-        chord_info[method.__name__] = chord
+    for name, shorthand in CHORD_TO_SHORTHAND.items():
+        chord_name = '{}{}'.format(root, shorthand)
+        chord = Chord(chord_name)
+        chord_info[name] = chord
     return chord_info
--- tests/test_chords.py
+++ tests/test_chords.py
@@ -17,7 +17,7 @@ class TestChord(unittest.TestCase):
             self.assertEqual(chord[ix], Note(note), msg)
 
     def test_constructor(self):
-        chord = Chord('C')
+        chord = Chord('CM')
         self.assertEqual(chord.notes, [Note('C4'),
                                        Note('E4'),
                                        Note('G4')])
@@ -59,202 +59,177 @@ class TestChord(unittest.TestCase):
             Chord._get_shorthand('fail')
 
     def test_major_triad(self):
-        chord = Chord.major_triad('C')
-        self.assertEqual(str(chord), 'CM')
-        self._chord_tester(chord, ['C4', 'E4', 'G4'])
-
-    def test_major_triad_on_Note(self):
-        chord = Chord.major_triad(Note('C'))
-        self.assertEqual(str(chord), 'CM')
-        self._chord_tester(chord, ['C4', 'E4', 'G4'])
-
-    def test_major_triad_on_int(self):
-        chord = Chord.major_triad(60)
+        chord = Chord('CM')
         self.assertEqual(str(chord), 'CM')
         self._chord_tester(chord, ['C4', 'E4', 'G4'])
 
     def test_minor_triad(self):
-        chord = Chord.minor_triad('C')
+        chord = Chord('Cm')
         self.assertEqual(str(chord), 'Cm')
         self._chord_tester(chord, ['C4', 'Eb4', 'G4'])
 
-    def test_diminished_triad_o(self):
-        chord = Chord.diminished_triad('C')
-        self.assertEqual(str(chord), 'Cdim')
-        self._chord_tester(chord, ['C4', 'Eb4', 'Gb4'])
-
     def test_diminished_triad(self):
-        chord = Chord.diminished_triad('C')
+        chord = Chord('Cdim')
         self.assertEqual(str(chord), 'Cdim')
         self._chord_tester(chord, ['C4', 'Eb4', 'Gb4'])
 
     def test_augmented_triad(self):
-        chord = Chord.augmented_triad('C')
+        chord = Chord('Caug')
         self.assertEqual(str(chord), 'Caug')
         self._chord_tester(chord, ['C4', 'E4', 'G#4'])
 
     def test_augmented_minor_seventh(self):
-        chord = Chord.augmented_minor_seventh('C')
+        chord = Chord('Cm7+')
         self.assertEqual(str(chord), 'Cm7+')
         self._chord_tester(chord, ['C4', 'E4', 'G#4', 'Bb4'])
 
     def test_augmented_major_seventh(self):
-        chord = Chord.augmented_major_seventh('C')
+        chord = Chord('CM7+')
         self.assertEqual(str(chord), 'CM7+')
         self._chord_tester(chord, ['C4', 'E4', 'G#4', 'B4'])
 
-    def test_suspended_triad(self):
-        chord = Chord.suspended_triad('C')
-        self.assertEqual(str(chord), 'Csus')
-        self._chord_tester(chord, ['C4', 'F4', 'G4'])
-
     def test_suspended_fourth_triad(self):
-        chord = Chord.suspended_fourth_triad('C')
+        chord = Chord('Csus4')
         self.assertEqual(str(chord), 'Csus4')
         self._chord_tester(chord, ['C4', 'F4', 'G4'])
 
     def test_suspended_second_triad(self):
-        chord = Chord.suspended_second_triad('C')
+        chord = Chord('Csus2')
         self.assertEqual(str(chord), 'Csus2')
         self._chord_tester(chord, ['C4', 'D4', 'G4'])
 
     def test_suspended_seventh(self):
-        chord = Chord.suspended_seventh('C')
+        chord = Chord('Csus47')
         self.assertEqual(str(chord), 'Csus47')
         self._chord_tester(chord, ['C4', 'F4', 'G4', 'Bb4'])
 
     def test_suspended_fourth_ninth(self):
-        chord = Chord.suspended_fourth_ninth('C')
+        chord = Chord('Csus4b9')
         self.assertEqual(str(chord), 'Csus4b9')
         self._chord_tester(chord, ['C4', 'F4', 'G4', 'Db5'])
 
     def test_eleventh(self):
-        chord = Chord.eleventh('C')
+        chord = Chord('C11')
         self.assertEqual(str(chord), 'C11')
         self._chord_tester(chord, ['C4', 'G4', 'Bb4', 'F5'])
 
     def test_minor_eleventh(self):
-        chord = Chord.minor_eleventh('C')
+        chord = Chord('Cm11')
         self.assertEqual(str(chord), 'Cm11')
         self._chord_tester(chord, ['C4', 'Eb4', 'G4', 'Bb4', 'F5'])
 
     def test_lydian_dominant_seventh(self):
-        chord = Chord.lydian_dominant_seventh('C')
+        chord = Chord('C7#11')
         self.assertEqual(str(chord), 'C7#11')
         self._chord_tester(chord, ['C4', 'E4', 'G4', 'Bb4', 'F#5'])
 
     def test_minor_thirteenth(self):
-        chord = Chord.minor_thirteenth('C')
+        chord = Chord('Cm13')
         self.assertEqual(str(chord), 'Cm13')
         self._chord_tester(chord, ['C4', 'Eb4', 'G4', 'Bb4', 'D5', 'A5'])
 
     def test_major_thirteenth(self):
-        chord = Chord.major_thirteenth('C')
+        chord = Chord('CM13')
         self.assertEqual(str(chord), 'CM13')
         self._chord_tester(chord, ['C4', 'E4', 'G4', 'B4', 'D5', 'A5'])
 
     def test_dominant_thirteenth(self):
-        chord = Chord.dominant_thirteenth('C')
+        chord = Chord('C13')
         self.assertEqual(str(chord), 'C13')
         self._chord_tester(chord, ['C4', 'E4', 'G4', 'Bb4', 'D5', 'A5'])
 
     def test_major_seventh(self):
-        chord = Chord.major_seventh('C')
+        chord = Chord('CM7')
         self.assertEqual(str(chord), 'CM7')
         self._chord_tester(chord, ['C4', 'E4', 'G4', 'B4'])
 
     def test_minor_seventh(self):
-        chord = Chord.minor_seventh('C')
+        chord = Chord('Cm7')
         self.assertEqual(str(chord), 'Cm7')
         self._chord_tester(chord, ['C4', 'Eb4', 'G4', 'Bb4'])
 
     def test_dominant_seventh(self):
-        chord = Chord.dominant_seventh('C')
-        self.assertEqual(str(chord), 'Cdom7')
+        chord = Chord('C7')
+        self.assertEqual(str(chord), 'C7')
         self._chord_tester(chord, ['C4', 'E4', 'G4', 'Bb4'])
 
     def test_diminished_seventh(self):
-        chord = Chord.diminished_seventh('C')
+        chord = Chord('Cdim7')
         self.assertEqual(str(chord), 'Cdim7')
         self._chord_tester(chord, ['C4', 'Eb4', 'Gb4', 'Bbb4'])
 
-    def test_half_diminished_seventh(self):
-        chord = Chord.half_diminished_seventh('C')
-        self.assertEqual(str(chord), 'Cm7b5')
-        self._chord_tester(chord, ['C4', 'Eb4', 'Gb4', 'Bb4'])
-
     def test_minor_seventh_flat_five(self):
-        chord = Chord.minor_seventh_flat_five('C')
+        chord = Chord('Cm7b5')
         self.assertEqual(str(chord), 'Cm7b5')
         self._chord_tester(chord, ['C4', 'Eb4', 'Gb4', 'Bb4'])
 
     def test_minor_major_seventh(self):
-        chord = Chord.minor_major_seventh('C')
-        self.assertEqual(str(chord), 'Cm/M7')
+        chord = Chord('CmM7')
+        self.assertEqual(str(chord), 'CmM7')
         self._chord_tester(chord, ['C4', 'Eb4', 'G4', 'B4'])
 
     def test_minor_sixth(self):
-        chord = Chord.minor_sixth('C')
+        chord = Chord('Cm6')
         self.assertEqual(str(chord), 'Cm6')
         self._chord_tester(chord, ['C4', 'Eb4', 'G4', 'A4'])
 
     def test_major_sixth(self):
-        chord = Chord.major_sixth('C')
+        chord = Chord('CM6')
         self.assertEqual(str(chord), 'CM6')
         self._chord_tester(chord, ['C4', 'E4', 'G4', 'A4'])
 
     def test_dominant_sixth(self):
-        chord = Chord.dominant_sixth('C')
+        chord = Chord('C67')
         self.assertEqual(str(chord), 'C67')
         self._chord_tester(chord, ['C4', 'E4', 'G4', 'A4', 'Bb4'])
 
     def test_sixth_ninth(self):
-        chord = Chord.sixth_ninth('C')
+        chord = Chord('C69')
         self.assertEqual(str(chord), 'C69')
         self._chord_tester(chord, ['C4', 'E4', 'G4', 'A4', 'D5'])
 
     def test_minor_ninth(self):
-        chord = Chord.minor_ninth('C')
+        chord = Chord('Cm9')
         self.assertEqual(str(chord), 'Cm9')
         self._chord_tester(chord, ['C4', 'Eb4', 'G4', 'Bb4', 'D5'])
 
     def test_major_ninth(self):
-        chord = Chord.major_ninth('C')
+        chord = Chord('CM9')
         self.assertEqual(str(chord), 'CM9')
         self._chord_tester(chord, ['C4', 'E4', 'G4', 'B4', 'D5'])
 
     def test_added_ninth(self):
-        chord = Chord.added_ninth('C')
+        chord = Chord('Cadd9')
         self.assertEqual(str(chord), 'Cadd9')
         self._chord_tester(chord, ['C4', 'E4', 'G4', 'D5'])
 
     def test_dominant_ninth(self):
-        chord = Chord.dominant_ninth('C')
+        chord = Chord('C9')
         self.assertEqual(str(chord), 'C9')
         self._chord_tester(chord, ['C4', 'E4', 'G4', 'Bb4', 'D5'])
 
     def test_dominant_flat_ninth(self):
-        chord = Chord.dominant_flat_ninth('C')
+        chord = Chord('C7b9')
         self.assertEqual(str(chord), 'C7b9')
         self._chord_tester(chord, ['C4', 'E4', 'G4', 'Bb4', 'Db5'])
 
     def test_dominant_sharp_ninth(self):
-        chord = Chord.dominant_sharp_ninth('C')
+        chord = Chord('C7#9')
         self.assertEqual(str(chord), 'C7#9')
         self._chord_tester(chord, ['C4', 'E4', 'G4', 'Bb4', 'D#5'])
 
     def test_dominant_flat_five(self):
-        chord = Chord.dominant_flat_five('C')
+        chord = Chord('C7b5')
         self.assertEqual(str(chord), 'C7b5')
         self._chord_tester(chord, ['C4', 'E4', 'Gb4', 'Bb4'])
 
     def test_hendrix_chord(self):
-        chord = Chord.hendrix_chord('C')
+        chord = Chord('C7b12')
         self.assertEqual(str(chord), 'C7b12')
         self._chord_tester(chord, ['C4', 'E4', 'G4', 'Bb4', 'Eb5'])
 
     def test_transpose_chord(self):
-        chord = Chord('C')
+        chord = Chord('CM')
         self.assertEqual(chord.notes, [Note('C'),
                                        Note('E4'),
                                        Note('G4')])
@@ -276,7 +251,8 @@ class TestAllChords(unittest.TestCase):
 
     def test_produce_all_chords(self):
 
-        expected = {'augmented_major_seventh': Chord('CM7+'),
+        expected = {'added_ninth': Chord('Cadd9'),
+                    'augmented_major_seventh': Chord('CM7+'),
                     'augmented_minor_seventh': Chord('Cm7+'),
                     'augmented_triad': Chord('Caug'),
                     'diminished_seventh': Chord('Cdim7'),
@@ -284,12 +260,11 @@ class TestAllChords(unittest.TestCase):
                     'dominant_flat_five': Chord('C7b5'),
                     'dominant_flat_ninth': Chord('C7b9'),
                     'dominant_ninth': Chord('C9'),
-                    'dominant_seventh': Chord('Cdom7'),
+                    'dominant_seventh': Chord('C7'),
                     'dominant_sharp_ninth': Chord('C7#9'),
                     'dominant_sixth': Chord('C67'),
                     'dominant_thirteenth': Chord('C13'),
                     'eleventh': Chord('C11'),
-                    'half_diminished_seventh': Chord('Cm7b5'),
                     'hendrix_chord': Chord('C7b12'),
                     'lydian_dominant_seventh': Chord('C7#11'),
                     'major_ninth': Chord('CM9'),
@@ -298,7 +273,7 @@ class TestAllChords(unittest.TestCase):
                     'major_thirteenth': Chord('CM13'),
                     'major_triad': Chord('CM'),
                     'minor_eleventh': Chord('Cm11'),
-                    'minor_major_seventh': Chord('Cm/M7'),
+                    'minor_major_seventh': Chord('CmM7'),
                     'minor_ninth': Chord('Cm9'),
                     'minor_seventh': Chord('Cm7'),
                     'minor_seventh_flat_five': Chord('Cm7b5'),
@@ -309,6 +284,5 @@ class TestAllChords(unittest.TestCase):
                     'suspended_fourth_ninth': Chord('Csus4b9'),
                     'suspended_fourth_triad': Chord('Csus4'),
                     'suspended_second_triad': Chord('Csus2'),
-                    'suspended_seventh': Chord('Csus47'),
-                    'suspended_triad': Chord('Csus')}
+                    'suspended_seventh': Chord('Csus47')}
         self.assertEqual(self.chord_info, expected)
